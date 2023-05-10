48fda681fb3498a2faa516f5a25c081ab42c57ac
Tatsuhiro Ikebe <beike315@icloud.com>
2023-05-10 18:19:58 +0900

    Feat find closest timestamp (#2)
    * Add dependent
    
    * Feature findClosestTimestamp

--- include/converged_radius_table_search/converged_radius_table_search.hpp
+++ include/converged_radius_table_search/converged_radius_table_search.hpp
@@ -4,7 +4,13 @@
 #ifndef CONVERGED_RADIUS_TABLE_SEARCH__CONVERGED_RADIUS_TABLE_SEARCH_HPP_
 #define CONVERGED_RADIUS_TABLE_SEARCH__CONVERGED_RADIUS_TABLE_SEARCH_HPP_
 
-#include "rclcpp/rclcpp.hpp"
+#include <queue>
+
+#include <rclcpp/rclcpp.hpp>
+
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <std_msgs/msg/header.hpp>
 
 namespace converged_radius_table_search {
 
@@ -13,7 +19,20 @@ public:
   ConvergedRadiusTableSearchNode();
 
 protected:
+  void initSubcription();
+  void ekfPoseCb(geometry_msgs::msg::PoseStamped::ConstSharedPtr msg);
+  void pointcloudCb(sensor_msgs::msg::PointCloud2::ConstSharedPtr msg);
+
+  rclcpp::Time findClosestTimestamp(std_msgs::msg::Header target);
+  void eraseEkfPoseQueue();
+
 private:
+  rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr
+      sub_ekf_pose_;
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr
+      sub_pointcloud_;
+
+  std::queue<geometry_msgs::msg::PoseStamped> ekf_pose_queue_;
 };
 
 } // namespace converged_radius_table_search
--- src/converged_radius_table_search.cpp
+++ src/converged_radius_table_search.cpp
@@ -5,7 +5,60 @@
 
 namespace converged_radius_table_search {
 ConvergedRadiusTableSearchNode::ConvergedRadiusTableSearchNode()
-    : Node("converged_radius_table_search_node") {}
+    : Node("converged_radius_table_search_node") {
+  initSubcription();
+}
+
+void ConvergedRadiusTableSearchNode::initSubcription() {
+  sub_ekf_pose_ = create_subscription<geometry_msgs::msg::PoseStamped>(
+      "/localization/pose_twist_fusion_filter/pose", 1,
+      std::bind(&ConvergedRadiusTableSearchNode::ekfPoseCb, this,
+                std::placeholders::_1));
+
+  sub_pointcloud_ = create_subscription<sensor_msgs::msg::PointCloud2>(
+      "/localization/util/downsample/pointcloud", 1,
+      std::bind(&ConvergedRadiusTableSearchNode::pointcloudCb, this,
+                std::placeholders::_1));
+}
+
+void ConvergedRadiusTableSearchNode::ekfPoseCb(
+    geometry_msgs::msg::PoseStamped::ConstSharedPtr msg) {
+  ekf_pose_queue_.push(*msg);
+}
+
+void ConvergedRadiusTableSearchNode::pointcloudCb(
+    sensor_msgs::msg::PointCloud2::ConstSharedPtr msg) {
+  findClosestTimestamp(msg->header);
+}
+
+rclcpp::Time ConvergedRadiusTableSearchNode::findClosestTimestamp(
+    std_msgs::msg::Header target) {
+  rclcpp::Time target_time(target.stamp);
+
+  double duration_sec_old = INFINITY;
+  while (!ekf_pose_queue_.empty()) {
+    rclcpp::Time ekf_pose_time(ekf_pose_queue_.front().header.stamp);
+    auto duration_sec_latest = (target_time - ekf_pose_time).seconds();
+
+    if (duration_sec_old != INFINITY) {
+      if (duration_sec_old > duration_sec_latest) {
+        return rclcpp::Time(ekf_pose_queue_.front().header.stamp);
+        break;
+      } else
+        ekf_pose_queue_.pop();
+    }
+    duration_sec_old = duration_sec_latest;
+  }
+
+  eraseEkfPoseQueue();
+
+  return rclcpp::Time();
+}
+
+void ConvergedRadiusTableSearchNode::eraseEkfPoseQueue() {
+  ekf_pose_queue_ = std::queue<geometry_msgs::msg::PoseStamped>();
+}
+
 } // namespace converged_radius_table_search
 
 int main(int argc, char *argv[]) {
